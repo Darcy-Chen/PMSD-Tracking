56e918e73557081a503b31d43175c6f740c4dfd9
Qilin Chen <qilin.chen@tum.de>
2024-08-03 16:06:35 +0200

    Update gitignore

--- SeqTrack/lib/test/evaluation/local.py
+++ SeqTrack/lib/test/evaluation/local.py
@@ -5,31 +5,31 @@ def local_env_settings():
 
     # Set your local paths here.
 
+    settings.custom_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/custom'
     settings.davis_dir = ''
-    settings.got10k_lmdb_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/got10k_lmdb'
-    settings.got10k_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/got10k'
+    settings.got10k_lmdb_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/got10k_lmdb'
+    settings.got10k_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/got10k'
     settings.got_packed_results_path = ''
     settings.got_reports_path = ''
-    settings.lasot_extension_subset_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/lasot_extension_subset'
-    settings.lasot_lmdb_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/lasot_lmdb'
-    settings.lasot_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/lasot'
-    settings.network_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/test/networks'    # Where tracking networks are stored.
-    settings.nfs_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/nfs'
-    settings.otb_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/OTB2015'
-    settings.prj_dir = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack'
-    settings.result_plot_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/test/result_plots'
-    settings.results_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/test/tracking_results'    # Where to store tracking results
-    settings.save_dir = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack'
-    settings.segmentation_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/test/segmentation_results'
-    settings.tc128_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/TC128'
+    settings.lasot_extension_subset_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/lasot_extension_subset'
+    settings.lasot_lmdb_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/lasot_lmdb'
+    settings.lasot_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/lasot'
+    settings.network_path = '/home/darcy/PMSD-Tracking/SeqTrack/test/networks'    # Where tracking networks are stored.
+    settings.nfs_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/nfs'
+    settings.otb_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/OTB2015'
+    settings.prj_dir = '/home/darcy/PMSD-Tracking/SeqTrack'
+    settings.result_plot_path = '/home/darcy/PMSD-Tracking/SeqTrack/test/result_plots'
+    settings.results_path = '/home/darcy/PMSD-Tracking/SeqTrack/test/tracking_results'    # Where to store tracking results
+    settings.save_dir = '/home/darcy/PMSD-Tracking/SeqTrack'
+    settings.segmentation_path = '/home/darcy/PMSD-Tracking/SeqTrack/test/segmentation_results'
+    settings.tc128_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/TC128'
     settings.tn_packed_results_path = ''
-    settings.tnl2k_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/tnl2k'
+    settings.tnl2k_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/tnl2k'
     settings.tpl_path = ''
-    settings.trackingnet_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/trackingnet'
-    settings.uav_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/UAV123'
-    settings.vot_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/VOT2019'
+    settings.trackingnet_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/trackingnet'
+    settings.uav_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/UAV123'
+    settings.vot_path = '/home/darcy/PMSD-Tracking/SeqTrack/data/VOT2019'
     settings.youtubevos_dir = ''
-    settings.custom_path = '/home/darcy/PycharmProjects/PMSD-Tracking/SeqTrack/data/custom'
 
     return settings
 
